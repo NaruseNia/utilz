4eec6c388eb5d10fbaa368b2c9ea14b2c374a296
NaruseNia <notshinsin0000@gmail.com>
2024-12-31 10:24:45 +0900

    Use tailwind and fix some styles

--- src/js/main/index.tsx
+++ src/js/main/index.tsx
@@ -5,6 +5,9 @@ import { darkTheme, Provider } from "@adobe/react-spectrum";
 
 import Main from "./main";
 
+import "../style/prelude.css";
+import "../style/index.css";
+
 initBolt();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -1,6 +1,6 @@
 const Main = () => {
   return (
-    <div>
+    <div className="h-screen">
       <h1 style={{ color: "#ff5b3b" }}>Welcome to Bolt CEP!</h1>
     </div>
   );
